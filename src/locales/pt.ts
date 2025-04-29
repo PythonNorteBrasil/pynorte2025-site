917c0bbe6f410e6ce9805bc953a5f45483e74466
tuiza <luiza8.marlene@gmail.com>
2025-04-29 00:25:05 -0300

    refactor: text review

--- src/locales/pt.ts
+++ src/locales/pt.ts
@@ -11,6 +11,7 @@ export default {
   // Component -> Contact
   "component.contact.title": "Nossos contatos",
   "component.contact.buyTicket": "Adquira seu Ingresso",
+
   // Component -> Footer
   "component.footer.aboutTheEvent": "Sobre o Evento",
   "component.footer.eventLocation": "Local do Evento",
@@ -18,67 +19,76 @@ export default {
   "component.footer.program": "Programação",
   "component.footer.questions": "Perguntas Frequentes",
   "component.footer.sponsors": "Patrocinadores",
-  "component.footer.submitTalk": "Submeter Palestra",
+  "component.footer.submitTalk": "Submeta sua Palestra",
+
   // Component -> Hero
   "component.hero.description":
-    "O maior evento de Python do região norte do Brasil. Junte-se a nós em Belém do Pará para 3 dias de aprendizado, networking e cultura amazôniza.",
+    "O maior evento de Python da região Norte do Brasil. Junte-se a nós em Belém do Pará para 3 dias de aprendizado, networking e cultura amazônica.",
   "component.hero.register": "Adquira seu Ingresso",
   "component.hero.countdown": "Contagem Regressiva",
   "component.hero.ourEventIsHere": "Chegou nosso encontro!",
-  "component.hero.watchKeynotes": "Assista a transmissão de nossos Keynotes!",
-  "component.hero.missItAlready": "Saudades maninho?",
+  "component.hero.watchKeynotes": "Assista à transmissão de nossos Keynotes!",
+  "component.hero.missItAlready": "Saudades, maninho?",
   "component.hero.accessRepository":
-    "Acesse nosso repositório e guarda de lembrança!",
+    "Acesse nosso repositório e guarde de lembrança!",
   "component.hero.photosAndMaterials": "Fotos e materiais de apoio",
   "component.hero.timer.days": "Dias",
+
   // Component -> Countdown Timer
   "component.hero.timer.hours": "Horas",
   "component.hero.timer.minutes": "Minutos",
   "component.hero.timer.seconds": "Segundos",
+
   // Component -> Location
   "component.location.title": "Local do Evento",
   "component.location.description":
-    "O Python Norte 2025 será realizado na Universidade da Amazônia (UNAMA) da Alcindo Cacela, proporcionando uma estrutura completa para o evento com fácil acesso. O campus Alcindo Cacela é um dos principais pontos de referência educacional de Belém.",
-  "component.location.maps.title": "UNAMA Alcindo Cacela Map",
+    "O Python Norte 2025 será realizado na Universidade da Amazônia (UNAMA), campus Alcindo Cacela, proporcionando uma estrutura completa para o evento com fácil acesso. O campus é um dos principais pontos de referência educacional de Belém.",
+  "component.location.maps.title": "Mapa da UNAMA Alcindo Cacela",
+
   // Component -> Sponsors
   "component.sponsors.title": "Patrocinadores",
-  "component.sponsors.description": "A receita para o sucesso é simples: pessoas > tecnologia + muito suporte! Para que esse evento aconteça com a qualidade que todos merecem, precisamos da colaboração de empresas como a sua! Se sua marca quer estar no topo da panela Python e fazer parte de um evento impactante no Norte do país, temos um lugar na nossa banca de tacacá só para você!",
-  "component.sponsors.button": "Cheque nosso Plano de Patrocínio",
+  "component.sponsors.description":
+    "A receita para o sucesso é simples: pessoas > tecnologia + muito suporte! Para que este evento aconteça com a qualidade que todos merecem, precisamos da colaboração de empresas como a sua! Se sua marca quer estar no topo da panela Python e fazer parte de um evento impactante no Norte do país, temos um lugar reservado na nossa banca de tacacá só para você!",
+  "component.sponsors.button": "Confira nosso Plano de Patrocínio",
+
   // Component -> FAQ
   "component.faq.title": "Perguntas Frequentes",
+
   // Component -> Schedule
   "component.schedule.title": "Programação",
+
   // Page -> Calendar
   "page.calendar.friday": "Sexta-feira, 4 de julho",
   "page.calendar.saturday": "Sábado, 5 de julho",
   "page.calendar.title": "Calendário do Evento",
+
   // Page -> Code of Conduct
   "page.codeOfConduct.title": "Código de Conduta",
   "page.codeOfConduct.introduction":
     "O evento Python Norte tem o compromisso de proporcionar um ambiente seguro, inclusivo e respeitoso para todas as pessoas envolvidas. Nosso objetivo é fomentar a troca de conhecimento e experiências de forma acolhedora e plural.",
   "page.codeOfConduct.introduction2":
-    "Para garantir um ambiente harmonioso, contamos com a colaboração de todas as pessoas presentes no evento. Com esta finalidade, a organização do evento conta com uma Equipe de Resposta que atua para garantir um ambiente com essas qualidades.",
+    "Para garantir um ambiente harmonioso, contamos com a colaboração de todas as pessoas presentes no evento. Para isso, a organização dispõe de uma Equipe de Resposta que atua para assegurar essas qualidades.",
   "page.codeOfConduct.rules.title": "Sendo assim:",
   "page.codeOfConduct.rules.1":
-    "Não é tolerado nenhum tipo de assédio, discriminação inapropriada ou humilhação pública;",
+    "Não será tolerado nenhum tipo de assédio, discriminação inadequada ou humilhação pública;",
   "page.codeOfConduct.rules.2":
-    "Não é tolerado o descumprimento das leis brasileiras;",
+    "É obrigatório o cumprimento das leis brasileiras;",
   "page.codeOfConduct.rules.3":
-    "Toda pessoa presente no evento, independentemente do seu papel, está sujeita a estas regras.",
+    "Todas as pessoas presentes no evento, independentemente do seu papel, estão sujeitas a estas regras.",
   "page.codeOfConduct.definitions.title": "Desta forma, entendemos que:",
   "page.codeOfConduct.definitions.1":
-    "Assédio é qualquer comportamento que viole o bom-humor, a intimidade ou a dignidade de outra pessoa.",
+    "Assédio é qualquer comportamento que intimide, constranja ou desrespeite outra pessoa.",
   "page.codeOfConduct.definitions.2":
-    "Discriminação inapropriada é qualquer comportamento que viole o bom-humor, a intimidade ou a dignidade de outra pessoa.",
+    "Discriminação inadequada é qualquer atitude que desvalorize ou exclua uma pessoa ou grupo com base em características pessoais.",
   "page.codeOfConduct.definitions.3":
-    "Humilhação pública é qualquer comportamento que viole o bom-humor, a intimidade ou a dignidade de outra pessoa.",
+    "Humilhação pública é a exposição vexatória de uma pessoa com o objetivo de constrangê-la ou ridicularizá-la.",
   "page.codeOfConduct.additionalInformation1":
-    "Como o evento pode incluir a participação de crianças e adolescentes, buscamos manter um ambiente apropriado para todas as faixas etárias. Por isso, linguagem e imagens sexualizadas não são adequadas para palestras, atividades e ações promocionais de patrocinadores.",
+    "Como o evento pode incluir a participação de crianças e adolescentes, buscamos manter um ambiente apropriado para todas as faixas etárias. Portanto, linguagem e imagens sexualizadas não são adequadas para palestras, atividades e ações promocionais de patrocinadores.",
   "page.codeOfConduct.additionalInformation2":
     "Se você se sentiu assediado(a), discriminado(a) ou humilhado(a), ou presenciou qualquer comportamento inadequado, pedimos que entre em contato com a Equipe de Resposta do Código de Conduta por meio do {link}.",
   "page.codeOfConduct.additionalInformation2.link": "formulário de contato",
   "page.codeOfConduct.additionalInformation3":
-    "Havendo um relato de violação destes princípios, a Equipe de Resposta realizará a devida análise e, quando necessário, tomará as ações para impedir a reincidência. Estas ações podem, mas não se restringem nem implicam em ir desde uma conversa em busca da retratação até um convite para se retirar do evento por tempo indeterminado.",
+    "Em caso de violação destes princípios, a Equipe de Resposta analisará a situação e tomará as medidas necessárias para evitar reincidência. As ações podem variar desde uma conversa até a retirada da pessoa do evento.",
   "page.codeOfConduct.closingStatements1":
     "Nosso compromisso é garantir que o Python Norte seja um espaço acolhedor, diverso e respeitoso para toda a comunidade.",
   "page.codeOfConduct.closingStatements2":
@@ -88,13 +98,15 @@ export default {
   "page.codeOfConduct.reportButton": "Enviar denúncia",
   "page.codeOfConduct.responseTeam.title":
     "Equipe de Resposta do Código de Conduta",
+
   // Page -> About the Event
   "page.aboutTheEvent.title": "Sobre o Evento",
   "page.aboutTheEvent.intro":
-    "Python Norte é o evento sobre a linguagem de programação Python voltado para a região norte do Brasil, apoiado pela Python Brasil Association (ApyB) e pela PSF (Python Software Foundation).",
+    "Python Norte é o evento sobre a linguagem de programação Python voltado para a região Norte do Brasil, apoiado pela Associação Python Brasil (ApyB) e pela Python Software Foundation (PSF).",
   "page.aboutTheEvent.intro2":
-    "Este ano, o evento acontecerá entre os dias  4 e 6 de julho na cidade de Belém, no Pará.",
+    "Este ano, o evento acontecerá entre os dias 4 e 6 de julho na cidade de Belém, no Pará.",
   "page.aboutTheEvent.codeOfConduct.button": "Leia nosso Código de Conduta",
+
   // Component -> EventInfos
   "component.aboutTheEvent.eventInfos.days": "Dias de Evento",
   "component.aboutTheEvent.eventInfos.experts": "Especialistas",
