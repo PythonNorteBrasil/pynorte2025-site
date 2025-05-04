8c8a97995c30cbfcb94d38c0570f67cc77117216
tuiza <luiza8.marlene@gmail.com>
2025-05-04 13:07:38 -0300

    feat: new colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,17 +28,21 @@ export default {
         "theme-background": "#f8f4b7", // Tacacá 01
 
         // One color from each palette
-        "acai": "#3b276b",    // Açaí 02
-        "vibora": "#057ca4",  // Víbora 02
+        acai: "#3b276b", // Açaí 02
+        vibora: "#057ca4", // Víbora 02
         "vibora-alternative": "#0a4d9d", // Víbora 04
-        "jambu": "#41501f", // Jambu 03
-        "jiboia": "#7d1d1d", // Jiboia 06
+        jambu: "#41501f", // Jambu 03
+        "jambu-alternative": "#D5DC8C", // Jambu 01
+        jiboia: "#7d1d1d", // Jiboia 06
         "jiboia-light": "#de3e2e", // Jiboia 02
-        "camarao": "#f28e89", // Camarão 01
+        camarao: "#f28e89", // Camarão 01
         "camarao-light": "#ed6954", // Camarão 02
-        "tacaca": "#f8f4b7",  // Tacacá 01
-        "tacaca-alternative": "#f6a118", // Tacacá 05
-        "cabocla": "#d75941", // Cabocla 01,
+        "camarao-alternative": "#F6D5A9",
+        tacaca: "#f8f4b7", // Tacacá 01
+        "tacaca-alternative": "#f6a118",
+        "tacaca-light": "#F6E9AE",
+        "cabloca-light": "#B59DCB4D",
+        cabocla: "#d75941", // Cabocla 01,
         "cabocla-alternative": "#351a1d", // Cabocla 07
 
         // shadcn/ui required colors
@@ -80,26 +84,41 @@ export default {
         mono: ["DM Mono", "monospace"],
       },
       fontSize: {
-        'h1': ['48pt', {
-          lineHeight: '18pt',
-          fontWeight: '300', // Light
-        }],
-        'h2': ['48pt', {
-          lineHeight: '18pt',
-          fontWeight: '500', // Medium
-        }],
-        'h3': ['30pt', {
-          lineHeight: '0pt',
-          fontWeight: '300', // Light
-        }],
-        'h4': ['30pt', {
-          lineHeight: '0pt',
-          fontWeight: '500', // Medium
-        }],
-        'h5': ['21pt', {
-          lineHeight: '0pt',
-          fontWeight: '400', // Regular
-        }],
+        h1: [
+          "48pt",
+          {
+            lineHeight: "18pt",
+            fontWeight: "300", // Light
+          },
+        ],
+        h2: [
+          "48pt",
+          {
+            lineHeight: "18pt",
+            fontWeight: "500", // Medium
+          },
+        ],
+        h3: [
+          "30pt",
+          {
+            lineHeight: "0pt",
+            fontWeight: "300", // Light
+          },
+        ],
+        h4: [
+          "30pt",
+          {
+            lineHeight: "0pt",
+            fontWeight: "500", // Medium
+          },
+        ],
+        h5: [
+          "21pt",
+          {
+            lineHeight: "0pt",
+            fontWeight: "400", // Regular
+          },
+        ],
       },
     },
   },
